6c80e7e9e0fe631e5654610237104e08ebd8873e
andrpbada525 <androbada525@gmail.com>
2019-11-24 07:07:42 +0000

    More source fixes for compilation
    asm/types.h wasn't present in include/; it was instead located at include/uapi/asm-generic/types.h, so I just updated the paths

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#0 SMP PREEMPT Sat Nov 23 19:27:10 UTC 2019"
-#define LINUX_COMPILE_BY "androbada525"
-#define LINUX_COMPILE_HOST "nightingdale"
-#define LINUX_COMPILER "gcc version 8.0.0 20170929 (prerelease) (UBERTC-8.x.x) "
